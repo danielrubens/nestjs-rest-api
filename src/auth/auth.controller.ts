6e9ee949f5f70351f1aa03c7ec3f7845b39d8f62
danielrubens <danielrubensdaniel@gmail.com>
2023-03-14 20:37:27 -0300

    chore: install class-validator and class-transformer

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('signup')
   signup(@Body() dto: AuthDto) {
-    console.log({ dto });
     return this.authService.signup();
   }
   @Post('signin')
