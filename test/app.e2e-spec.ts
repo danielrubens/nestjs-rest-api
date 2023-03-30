09f9e8f970e8aa4d6a91db2025aa1fc1ed461a50
danielrubens <danielrubensdaniel@gmail.com>
2023-03-30 00:25:01 -0300

    test: import request making library pactum

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
 import { PrismaService } from '../src/prisma/prisma.service';
 import { AppModule } from '../src/app.module';
 
@@ -20,8 +21,12 @@ describe('App e2e', () => {
     app.close();
   });
   describe('Auth', () => {
-    describe('Signup', () => {})
-    describe('Signin', () => {})
+    describe('Signup', () => {
+      it.todo('should signup')
+    })
+    describe('Signin', () => {
+      it.todo('should signin')
+    })
   })
   describe('User', () => {
     describe('Get me', () => {})
